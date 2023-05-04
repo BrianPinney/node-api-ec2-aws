c6fd0afe687d02007e6d0c6c33f9f5b9d7bf9874
Brian J. Pinney <brianj.pinney@gmail.com>
2023-05-04 16:00:04 -0400

    new data

--- index.js
+++ index.js
@@ -8,6 +8,30 @@ const PORT = 4000
 app.use(cors())
 app.use(express.json())
 
+const dogs = [
+    {
+    name: "Paco",
+    breed: 'Chiuahua',
+    age: 34
+},
+    {
+    name: "buba",
+    breed: 'Lab',
+    age: 23
+},
+    {
+    name: "Jericho",
+    breed: 'Doberman',
+    age: 2
+},
+    {
+    name: "Phoenix",
+    breed: 'Husky',
+    age: 7
+}
+
+]
+
 app.get('/', (req,res) => res.send('Welcome to my API'))
 
 app.listen(PORT, () => console.log(`API is running on port: ${PORT}`))
